3c5323f564ddfed44723bd12152e66b3a941ff83
soxxerr <159965015+soxxerr@users.noreply.github.com>
2024-07-15 08:46:35 -0400

    routing

--- js/next.js
+++ js/next.js
@@ -13,17 +13,49 @@ function setPageTitle(title){
     document.title = title;
 }
 
-// 
-// jquery 
-$('#rangestart').calendar({ type: 'date', endCalendar: $('#rangeend') }); $('#rangeend').calendar({ type: 'date', startCalendar: $('#rangestart') });
 
-$.modal({
-    title: 'Important Notice',
-    class: 'mini',
-    closeIcon: true,
-    content: 'You will be logged out in 5 Minutes',
-    actions: [{
-      text: 'Alright, got it',
-      class: 'green'
-    }]
-}).modal('show');
+// validation for sigup form 
+const Email = document.querySelector("#appl_id");
+        const welcom = document.querySelector(".hey");
+        const pass = document.querySelector("#appl_pass")
+        const domain = ["@mail.com", "@hotmail.com", "@gmai.com", "@apple.com","@icloud.com"];
+
+
+        Email.addEventListener("input", () => {
+            const firstvalue = Email.value;
+            const atIndex = firstvalue.indexOf("@");
+            const midstring = atIndex !== -1 ? firstvalue.substring(atIndex) : "";
+            
+            if (firstvalue.trim() === "") {
+                welcom.innerHTML = "Login to your apple account";
+                welcom.style.color = "red"
+                pass.style.display ="none"
+                Email.style.borderColor = "red";
+            } else if (!domain.includes(midstring)) {
+                welcom.innerHTML = `Invalid Apple Account.`;
+                Email.style.borderColor = "red";
+                welcom.style.color = "red"
+                pass.style.display ="none"
+            } else {
+                welcom.innerHTML = `Enter Password For ${firstvalue}`;
+                Email.style.borderColor = "green";
+                 welcom.style.color = "gray"
+                pass.style.display ="block"
+            }
+            console.log(firstvalue);
+        });
+        // validation for sigup form 
+
+        // 
+// jquery 
+
+// $.modal({
+//     title: 'Important Notice',
+//     class: 'mini',
+//     closeIcon: true,
+//     content: 'You will be logged out in 5 Minutes',
+//     actions: [{
+//       text: 'Alright, got it',
+//       class: 'green'
+//     }]
+// }).modal('show');
